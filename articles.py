5fc07a8023c7b40438d370a88b6bf3a2c2db0ea1
Adrian Pasik <adrian.pasik@vp.pl>
2021-08-07 22:48:28 +0200

    working parser

--- articles.py
+++ articles.py
@@ -1,35 +1,38 @@
 from newspaper import Article
+import sys
+from datetime import datetime
+import logging
 import couchdb
-url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
-couch = couchdb.Server('http://admin:couch8admin@127.0.0.1:5984/')
-db = couch['newsqa']
-doesEntityExist = db.get(url)
-if doesEntityExist is None:
-	db.save({'_id': url, 'text': 'dummy'})
-else:
-	print('Entity exists')
-
-
-
-""" url = 'http://fox13now.com/2013/12/30/new-year-new-laws-obamacare-pot-guns-and-drones/'
-article = Article(url2)
-article.download()
-article.parse() """
-
-
-# print(article.text)
-
 
 class ArticleSaver:
-	@staticmethod
-	def Process(url):
-		article = Article(url)
-		article.download()
-		article.parse().replace('\n',"").replace('\t',"")
-	def Save(couchDbHandle, url, content):
-		couchDbHandle.save({'_id': url, 'text': content})
+    def __init__(self, couchDbHandle) -> None:
+        self.couchDbHandle = couchDbHandle
+        self.logger = logging.getLogger('main')
+        pass
+    def Process(self, url):
+        try:
+            article = Article(url)
+            article.download()
+            article.parse()
+            self.logger.info('Processing succeded on url {}.'.format(url))
+            return article.text.replace('\n', "").replace('\t', "")
+        except Exception as e:
+            self.logger.error('Processing failure - will return empty url. Failed url {}. Exception {}'.format(url, e.message))
+            return ""
 
+    def Save(self, url, content):
+        try:
+            self.couchDbHandle.save({'_id': url, 'text': content})
+            self.logger.info('Saved successfully url {}.'.format(url))
+        except Exception as e:
+            self.logger.error('Error when saving in db. Failed url {}. Exception {}'.format(url, e.message))
 
-
-
-		
+    def DoesArticleExist(self, url):
+        try:
+            entry = self.couchDbHandle.get(url)
+            if entry is None:
+                return False
+            else:
+                return True
+        except Exception as e:
+            self.logger.error('Something wrong happened when checking for id. Exception {}'.format(e.message))
--- rss-parser.py
+++ rss-parser.py
@@ -1,6 +1,47 @@
+from bs4 import element
 import feedparser
-from articles import 
-rss = 'http://www.blog.pythonlibrary.org/feed/'
-feed = feedparser.parse(rss)
-for key in feed["entries"]: 
-    print(key["link"])
+import logging
+import couchdb
+import sys
+import random
+import time
+
+from newspaper import article
+from articles import ArticleSaver
+from datetime import datetime
+
+logger = logging.getLogger('main')
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    level=logging.INFO,
+                    datefmt='%Y-%m-%d %H:%M:%S')
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                              datefmt='%Y-%m-%d %H:%M:%S')
+fileHandler = logging.FileHandler(
+    'logs\{:%Y-%m-%d-%H-%M-%S}.log'.format(datetime.now()))
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+
+couch = couchdb.Server('http://admin:couch8admin@127.0.0.1:5984/')
+db = couch['newsqa']
+
+articleSaver = ArticleSaver(db)
+
+rss = ["http://www.blog.pythonlibrary.org/feed/"]
+for rssKey in rss:
+    logger.info('Processing RSS key {}'.format(rssKey))
+    feed = feedparser.parse(rssKey)
+    logger.info('Found {} keys in RSS'.format(len(feed["entries"])))
+    for key in feed["entries"]:
+        link = key["link"]
+        logger.info('Processing {element}'.format(element=link))
+        if articleSaver.DoesArticleExist(link):
+            logger.info('URL {} already exist, exiting'.format(link))
+        else:
+            logger.info('URL {} does not exist. Processing'.format(link))
+            sleepSeconds = random.randint(15, 40)
+            logger.info('Sleeping for {} seconds.'.format(sleepSeconds))
+            time.sleep(sleepSeconds)
+            content = articleSaver.Process(link)
+            articleSaver.Save(link, content)
+            
+
